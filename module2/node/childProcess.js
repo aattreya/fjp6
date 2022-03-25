bc1944a1d5985d25c5421d38efa49a800f7f3967
aattreya <barthwal.aattreya@gmail.com>
2022-03-25 13:05:40 +0530

    test file added

--- module2/node/childProcess.js
+++ module2/node/childProcess.js
@@ -1,5 +1,9 @@
 let cp = require('child_process');
 // console.log(cp)
 
-// opening a different file
-cp.execFileSync("calc");
+// opening calculator via node
+cp.execSync("calc");
+
+// executing a diff file via cp
+let content = cp.execSync("node test.js");
+console.log("Output of test.js :- " + content);
