f3e660a6d2afa11ffec9f452ade08b4f71655552
aattreya <barthwal.aattreya@gmail.com>
2022-03-30 02:50:45 +0530

    10 mar CRUD using fs

--- module2/node/fs.js
+++ module2/node/fs.js
@@ -3,7 +3,34 @@ let fs = require("fs");
 
 let path = require("path");
 let filePath = path.join(__dirname, "file.txt");
-console.log(filePath);
+// console.log(filePath);
 
+// C - create
 // creates a file if not already existing else overwrites the data
-fs.writeFileSync(filePath, "Hello i am a text file");
+fs.writeFileSync(filePath, "Hello i am a text file ");
+
+// R - read
+console.log("Before update : ")
+let content = fs.readFileSync(filePath, 'utf-8');
+console.log(content);
+
+// U - update
+fs.appendFileSync(filePath, "\nNewly added content");
+console.log("After update");
+console.log(fs.readFileSync(filePath, 'utf-8'));
+
+// D- delete
+fs.unlinkSync(filePath);
+
+// CRUD on directory
+let filePathDir = path.join(__dirname, "temp");
+console.log(filePathDir);
+
+// C- create
+fs.mkdirSync(filePathDir);
+
+// R - create
+fs.readdirSync(filePathDir);
+
+// D - delete
+fs.rmdirSync(filePathDir);
