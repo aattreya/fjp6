cb6759c4f24cf7179c74a6e51d38fd2507dfbebd
aattreya <barthwal.aattreya@gmail.com>
2022-03-30 03:06:38 +0530

    10 mar module

---
+++ module2/node/module/testModule.js
@@ -0,0 +1,7 @@
+let calc = require("./calculator.js");
+console.log(calc);
+
+console.log(calc.addition(5,6));
+console.log(calc.subtraction(5,6));
+console.log(calc.multiply(5,6));
+console.log(calc.divide(5,6));
