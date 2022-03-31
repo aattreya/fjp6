4a9e7106e4a00a035ea79f632575891304d9285e
aattreya <barthwal.aattreya@gmail.com>
2022-03-31 14:24:00 +0530

    input

---
+++ module2/project/fileOrganizer.js
@@ -0,0 +1,7 @@
+// console.log("checking whether the terminal is working correctly");
+
+// taking input from command line
+// index 0 - path where node is installed
+// index 1 - path of file 
+let input = process.argv[2];
+console.log(input);
