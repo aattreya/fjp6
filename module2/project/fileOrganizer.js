b8a0c2eba6d54a3f2f6be65c3a657f94e828f9a7
aattreya <barthwal.aattreya@gmail.com>
2022-03-31 14:40:48 +0530

    read folder

--- module2/project/fileOrganizer.js
+++ module2/project/fileOrganizer.js
@@ -1,7 +1,17 @@
-// console.log("checking whether the terminal is working correctly");
+let fs = require("fs");
+let path = require("path");
 
-// taking input from command line
-// index 0 - path where node is installed
-// index 1 - path of file 
-let input = process.argv[2];
-console.log(input);
+// Task 1 - taking input
+// asuumption - no folder inside folder
+let folderPath = process.argv[2];
+// console.log(folderPath);
+
+// Task 2 - read folder
+let folderExists = fs.existsSync(folderPath);
+if(folderExists){
+    let files = fs.readdirSync(folderPath);
+    console.log(files);
+}else{
+    console.log("Please enter a Valid Path !!!!!!!");
+}
+
